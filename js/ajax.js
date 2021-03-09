093f99360faaec235665ec892780db8e0a88dd78
Alexis Tinti <tintialexis@gmail.com>
2021-03-09 20:55:56 -0300

    TP Final + AJAX

--- js/ajax.js
+++ js/ajax.js
@@ -1,40 +1,24 @@
-/*
-$('#btnCargar').click(function(){
-    var esperar = 1000;
-    $.ajax({
 
-            url: "informacion.html",
-            beforeSend : function(){
-                $('#informacion').text('Cargando...');
-            } ,
-            success : function(data){
-                setTimeout(function(){
-                    $('#informacion').html(data);
-                }, esperar
-                );
-            }
-    });
-});
-*/
-console.log("esta trayendo bie el ava");
 function validacionMail(){
-var email = $("#input_mail").val();
-console.log(email);
+    var email = $("#input_mail").val();
 
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://mailcheck.p.rapidapi.com/?domain=mailinator.com",
+        "url": "https://mailcheck.p.rapidapi.com/?domain="+email,
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "45dbd56291msh0e520567a6b88dap1e4d69jsn5ca377c40553",
             "x-rapidapi-host": "mailcheck.p.rapidapi.com"
         }
     };
-    /*
+    
     $.ajax(settings).done(function (response) {
-        console.log(response);
+        if(response.valid) {
+            $("#formulario").submit();
+        } else {
+            $("#alerta").text("El email ingresado no es válido");
+        }
     });
-*/
-}
+  }
     
